4088c5d9465c198f313dfaaecd1a0a13f59b7852
Nick Rodriguez <nickbenrodriguez@gmail.com>
2018-10-15 19:31:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+const getFirstSelector = (selector) => {
+ return document.querySelector(selector);
 
+};
+
+const nestedTarget = () => {
+  return document.querySelector('#nested .target');
+};
+
+const deepestChild = () => {
+  let node = document.querySelector('#grand-node');
+  let next = node.querySelector('div');
+  while (next) {
+    node = next;
+    next = node.querySelector('div');
+  }
+  return node;
+};
+
+const increaseRankBy = (n) => {
+  const targets = document.querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < targets.length; i++) {
+    targets[i].innerHTML = parseInt(targets[i].innerHTML) + n;
+  }
+};
